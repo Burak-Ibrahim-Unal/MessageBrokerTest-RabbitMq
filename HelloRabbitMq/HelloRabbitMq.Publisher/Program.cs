53907a3bfeb62dea4f81a968498c6059338ff71f
Burak Ibrahim Unal <32199163+Burak-Ibrahim-Unal@users.noreply.github.com>
2024-05-16 11:43:31 +0300

    direct exchange

--- HelloRabbitMq/HelloRabbitMq.Publisher/Program.cs
+++ HelloRabbitMq/HelloRabbitMq.Publisher/Program.cs
@@ -2,6 +2,8 @@
 using RabbitMQ.Client;
 using System.Text;
 
+
+
 var factory = new ConnectionFactory();
 factory.Uri = new Uri("amqps://qjozhkni:2aIy943MANWa6PCklK21ZulOvrqogC0g@roedeer.rmq.cloudamqp.com/qjozhkni");
 
@@ -9,18 +11,38 @@ using var connection = factory.CreateConnection();
 var channel = connection.CreateModel();
 
 //durable:true = records all messages physically... We dont lose any data if we restart app.AFter messages sent,If there is not ony consumer,messages are lost
-channel.ExchangeDeclare("logs-fanout", durable: true, type: ExchangeType.Fanout);
+channel.ExchangeDeclare("logs-direct", durable: true, type: ExchangeType.Direct);
+
+Enum.GetNames(typeof(LogNames)).ToList().ForEach(x =>
+            {
+                var routeKey = $"route-{x}";
+                var queueName = $"direct-queue-{x}";
+                channel.QueueDeclare(queueName, true, false, false); //let differenct channels connect
+                channel.QueueBind(queueName, "logs-direct", routeKey, null);
+            });
+
+
 
 Enumerable.Range(1, 50).ToList().ForEach(x =>
-{
-    string message = $"Fanout log no: {x}";
-    var messageBody = Encoding.UTF8.GetBytes(message);
+            {
 
-    channel.BasicPublish("logs-fanout", "", null, messageBody);
+                LogNames log = (LogNames)new Random().Next(1, 5);
 
-    Console.WriteLine($"Message is sent {message}");
-});
+                string message = $"log-type: {log}";
+
+                var messageBody = Encoding.UTF8.GetBytes(message);
+
+                var routeKey = $"route-{log}";
+
+                channel.BasicPublish("logs-direct", routeKey, null, messageBody);
+
+                Console.WriteLine($"Log is sent {message}");
+            });
 
 
 Console.ReadLine();
 
+public enum LogNames
+{
+    Critical = 1, Error = 2, Warning = 3, Info = 4,
+}
